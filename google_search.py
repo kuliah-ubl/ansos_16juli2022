d68856431f70c4f11bef59b1c6e139c7344902bc
Muhamad Syafii <muhamad.syafii@moladin.com>
2022-07-16 19:52:41 +0700

    finish for google search

--- google_search.py
+++ google_search.py
@@ -8,19 +8,14 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+
 #set chromedriver.exe path
 # driver = webdriver.Chrome(executable_path='../drivers/chromedriver') #this is not use
 driver = webdriver.Chrome(ChromeDriverManager().install()) #using code here
 #url launch
 driver.get("http://google.com")
 #identify edit box with name
-# l = driver.find_element_by_link_text('Instituto') #not use this 
-driver.find_element(By.ID, "lst-ib").clear()
-driver.find_element(By.ID, "lst-ib").send_keys('hello world')
-#input text
-# l.send_keys('Selenium Python')
-#obtain value entered
-# v = l.get_attribute('value')
-# print('Value entered: ' + v)
-#driver close
+driver.implicitly_wait(30)
+driver.find_element(By.NAME, "q").send_keys('hello world')
+driver.find_element(By.NAME, "btnK").click()
 driver.close()
